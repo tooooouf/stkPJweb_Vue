6bd3d37982d5eceaed8e8d3c794388520b1ba028
tooouf <ne191024@senshu-u.jp>
2021-12-04 01:48:58 +0900

    プレレンダリングの実装

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import Index from '@/views/index.vue'
 import Akuryoubuster from '@/views/akuryoubuster.vue'
 
@@ -16,7 +16,7 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createWebHashHistory(),
+    history: createWebHistory(process.env.BASE_URL),
     base: /~proj2021-16/,
     routes,
     scrollBehavior() {
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,23 @@
+const path = require('path')
+const Prerender = require('prerender-spa-plugin')
+
 module.exports = {
     publicPath: '/~proj2021-16/',
+
+    configureWebpack: () => {
+        return {
+            plugins: [
+                new Prerender({
+                    staticDir: path.join(__dirname, 'dist'),
+
+                    outputDir: path.join(__dirname, 'dist'),
+
+                    indexPath: path.join(__dirname, "dist/index.html"),
+
+                    routes: [ '/', '/akuryoubuster' ]
+                })
+            ]
+        }
+    }
+
 };
